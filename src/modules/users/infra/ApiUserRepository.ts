2d21f0393c13e742d65e1081958bbdbe8edf3481
Carlos Azaustre <cazaustre@gmail.com>
2023-04-18 21:16:45 +0200

    perf: :zap: add memoization to requests on user repository

--- src/modules/users/infra/ApiUserRepository.ts
+++ src/modules/users/infra/ApiUserRepository.ts
@@ -4,22 +4,36 @@ import { UserRepository } from '../domain/UserRepository';
 const JSONPLACEHOLDER_URL = 'https://jsonplaceholder.typicode.com';
 
 export function createApiUserRepository(): UserRepository {
+  const cache: Map<number, User> = new Map();
+
+  async function get(id: number): Promise<User | null> {
+    if (cache.has(id)) {
+      return cache.get(id) as User;
+    }
+
+    const response = await fetch(`${JSONPLACEHOLDER_URL}/users/${id}`);
+    const user = await response.json();
+
+    cache.set(id, user);
+
+    return user;
+  }
+
+  async function getAll(): Promise<User[]> {
+    if (cache.size > 0) {
+      return Array.from(cache.values());
+    }
+
+    const response = await fetch(`${JSONPLACEHOLDER_URL}/users`);
+    const users = await response.json();
+
+    users.forEach((user: User) => cache.set(user.id, user));
+
+    return users;
+  }
+
   return {
     get,
     getAll,
   }
-}
-
-async function get(id: number): Promise<User | null> {
-  const response = await fetch(`${JSONPLACEHOLDER_URL}/users/${id}`);
-  const user = await response.json() as Promise<User>;
-
-  return user;
-}
-
-async function getAll(): Promise<User[]> {
-  const response = await fetch(`${JSONPLACEHOLDER_URL}/users`);
-  const users = await response.json() as Promise<User[]>;
-
-  return users;
 }
