5612c7598585b63621a79b61ae4f03ddeae6a84d
Carlos Azaustre <cazaustre@gmail.com>
2023-04-20 18:07:02 +0200

    test: :test_tube: configure tests

---
+++ tests/add.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it}  from 'vitest';
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    // speed up since tests don't rely on css
+    // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts#L14-L16
+    css: false,
+  },
+});
